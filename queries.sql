8e3c54a2b37d9a5344d63b0c8cb57abb4206e166
Stas Okhrym <s.okhrym@gmail.com>
2023-01-19 13:12:53 +0200

    Refactored queries

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT
     name,
-    (SELECT COUNT(link) FROM articles WHERE name=articles.name) as link_count
+    (SELECT COUNT(link) FROM articles WHERE link=articles.name) as link_count
 FROM articles
 GROUP BY name
 ORDER BY link_count DESC
@@ -17,3 +17,17 @@ ORDER BY link_count DESC
 LIMIT 5;
 
 -- 3. For a given article, find the average number of descendants of the second level:
+
+WITH first_level_links AS (
+    SELECT link
+    FROM articles
+    WHERE name = 'article.name'
+), second_level_links AS (
+    SELECT COUNT(second.link) AS descendant_count
+    FROM articles first
+    JOIN articles second ON first.link = second.name
+    WHERE first.name IN (SELECT link FROM first_level_links)
+    GROUP BY first.link
+)
+SELECT AVG(descendant_count) AS avg_descendants
+FROM second_level_links;
